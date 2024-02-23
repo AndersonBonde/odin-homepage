3ce77d0c4cc4aaa457f7df1290468c44757178ce
Anderson <andersonbonde@gmail.com>
2024-02-23 12:02:21 -0300

    Add page header for desktop size

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 This repo is a The Odin Project(TOP) exercise to implement a basic homepage to use as a portifolio in the future.
 
 The goal is to create a responsive page that can work on 3 different sizes of screens(desktop, tablet and cellphones).
+
+SVG Background
+
+<a href="https://www.svgbackgrounds.com/set/free-svg-backgrounds-and-patterns/">Free SVG Backgrounds and Patterns by SVGBackgrounds.com</a>
+
+Images
+
+Photo by Lisa Fotios: https://www.pexels.com/photo/white-rolling-armchair-beside-table-1957478/
+
+Photo by Tranmautritam: https://www.pexels.com/photo/nescafe-cup-near-imac-225502/
--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 import './style.css';
 import '@fortawesome/fontawesome-free/js/fontawesome';
 import '@fortawesome/fontawesome-free/js/solid';
+
+import headerBGImage from './images/large-triangles.svg';
+import headerMainImage from './images/pexels-tranmautritam-225502.jpg';
+
+(function loadHeaderBackground() {
+  const pageHeader = document.querySelector('body > header');
+
+  pageHeader.style.backgroundImage = `url("${headerBGImage}")`;
+}());
+
+(function loadHeaderMainImage() {
+  const headerImageContainer = document.querySelector('.header-image-container');
+  const newImage = document.createElement('img');
+
+  newImage.src = headerMainImage;
+  newImage.classList.add('headerImage');
+  headerImageContainer.appendChild(newImage);
+}());
+
+(function addH1ToHeaderImage() {
+  const headerImageContainer = document.querySelector('.header-image-container');
+  const newH1 = document.createElement('h1');
+
+  newH1.textContent = 'Lorem Ipsum Dolor';
+  newH1.classList.add('author-name');
+  headerImageContainer.appendChild(newH1);
+}());
