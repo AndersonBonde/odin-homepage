5041c2971b9f106cc3f7ef9e6e5e79c2c279fbc2
Anderson <andersonbonde@gmail.com>
2024-02-27 10:53:02 -0300

    Test image fix

--- src/index.js
+++ src/index.js
@@ -5,9 +5,16 @@ import '@fortawesome/fontawesome-free/js/solid';
 import '@fortawesome/fontawesome-free/js/brands';
 
 import headerBGImage from './images/large-triangles.svg';
+import headerImg from './images/pexels-tranmautritam-225502.jpg';
 
 (function loadHeaderBackground() {
   const pageHeader = document.querySelector('body > header');
 
   pageHeader.style.backgroundImage = `url("${headerBGImage}")`;
+}());
+
+(function loadPageImages() {
+  const headerImage = document.querySelector('.headerImage');
+
+  headerImage.src = headerImg;
 }());
